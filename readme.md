b44ec35c843b789c176b64968452ee364b4fd28c
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2021-05-30 03:18:50 +0200

    feat: switch to own byte protocol
    Pixelflut would be fun but was just too slow.

--- readme.md
+++ readme.md
@@ -1,7 +1,41 @@
-# ESP Pixelflut I2S LED Matrix
-
-I2S LED Matrix controllable via [Pixelflut Protocol](https://github.com/defnull/pixelflut).
+# ESP WLAN I2S LED Matrix
 
 Tested with [this I2S Shield](https://github.com/witnessmenow/ESP32-i2s-Matrix-Shield).
 
-Available on `tcp://espPixelflut:1337`.
+## Protocol
+
+When connecting you get 2 bytes (8 bit unsigned): width and height.
+Nothing is sent afterwards.
+
+Now you can send your commands.
+The first byte indicates the command you want to send.
+Bytes after that are specific to the command.
+Each byte is 8 bits unsigned.
+
+See this pseudocode for the available commands:
+
+```rust
+/// Fill the whole matrix with one color
+fn fill(red: u8, green: u8, blue: u8) {
+    client.send([1, red, green, blue]);
+}
+
+/// Set one pixel of the matrix to the given color
+fn pixel(x: u8, y: u8, red: u8, green: u8, blue: u8) {
+    client.send([2, x, y, red, green, blue]);
+}
+```
+
+There is a [Rust client library](https://github.com/EdJoPaTo/esp-wlan-led-matrix-rust-client) available.
+
+## MQTT
+
+`bri`ghtness and `on` can be controlled via MQTT (`<name>/set/bri`).
+
+Also, some status updates are published to MQTT (`<name>/status/bri`):
+- `bri`
+- `clients`
+- `commands-per-second`
+- `kilobytes-per-second`
+- `on`
+- `rssi`
--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,8 @@
 #include <LinkedList.h>
 #include <MqttKalmanPublish.h>
 
-#define CLIENT_NAME "espPixelflut"
+#define CLIENT_NAME "espPixelmatrix"
+const uint16_t LISTEN_PORT = 1337;
 
 EspMQTTClient client(
   WIFI_SSID,
@@ -23,30 +24,30 @@ const bool MQTT_RETAINED = true;
 #define BASIC_TOPIC_SET BASIC_TOPIC "set/"
 #define BASIC_TOPIC_STATUS BASIC_TOPIC "status/"
 
-
 // Configure for your panel(s) as appropriate!
-#define PANEL_WIDTH 64
-#define PANEL_HEIGHT 64
-#define PANELS_NUMBER 1
-#define PIN_E 18
+const uint16_t PANEL_WIDTH = 64;
+const uint16_t PANEL_HEIGHT = 64;
+const uint16_t PANELS_NUMBER = 1;
+const int8_t PIN_E = 18;
 
-#define PANE_WIDTH PANEL_WIDTH * PANELS_NUMBER
-#define PANE_HEIGHT PANEL_HEIGHT
+const uint16_t PANE_WIDTH = PANEL_WIDTH * PANELS_NUMBER;
+const uint16_t PANE_HEIGHT = PANEL_HEIGHT;
 
 // placeholder for the matrix object
 MatrixPanel_I2S_DMA *dma_display = nullptr;
 
-
-WiFiServer pixelflutServer(1337);
-LinkedList<WiFiClient> pixelflutClients;
+WiFiServer server(LISTEN_PORT);
+LinkedList<WiFiClient> clients;
 
 MQTTKalmanPublish mkCommandsPerSecond(client, BASIC_TOPIC_STATUS "commands-per-second", false, 12 * 1 /* every 1 min */, 10);
+MQTTKalmanPublish mkKilobytesPerSecond(client, BASIC_TOPIC_STATUS "kilobytes-per-second", false, 12 * 1 /* every 1 min */, 10);
 MQTTKalmanPublish mkRssi(client, BASIC_TOPIC_STATUS "rssi", MQTT_RETAINED, 12 * 5 /* every 5 min */, 10);
 
 boolean on = true;
-uint8_t mqttBri = 100;
+uint8_t mqttBri = 2;
 
 uint32_t commands = 0;
+uint32_t bytes = 0;
 int lastPublishedClientAmount = 0;
 
 void setup() {
@@ -58,57 +59,53 @@ void setup() {
   mxconfig.chain_length = PANELS_NUMBER;
   mxconfig.gpio.e = PIN_E;
   dma_display = new MatrixPanel_I2S_DMA(mxconfig);
-  dma_display->setBrightness8(mqttBri); // range is 0-255, 0 - 0%, 255 - 100%
+  dma_display->setBrightness8((mqttBri << 3)); // 0-7, 8-15, ... seem to be equal anyway
 
   // Allocate memory and start DMA display
-  if( not dma_display->begin() ) {
+  if (not dma_display->begin()) {
     Serial.println("****** !KABOOM! I2S memory allocation failed ***********");
   }
 
   ArduinoOTA.setHostname(CLIENT_NAME);
 
-  #ifdef PRINT_TO_SERIAL
+#ifdef PRINT_TO_SERIAL
   client.enableDebuggingMessages();
-  #endif
+#endif
   client.enableHTTPWebUpdater();
   client.enableLastWillMessage(BASIC_TOPIC "connected", "0", MQTT_RETAINED);
 
   // well, hope we are OK, let's draw some colors first :)
   Serial.println("Fill screen: RED");
   dma_display->fillScreenRGB888(255, 0, 0);
-  delay(1000);
+  delay(200);
 
   Serial.println("Fill screen: GREEN");
   dma_display->fillScreenRGB888(0, 255, 0);
-  delay(1000);
+  delay(200);
 
   Serial.println("Fill screen: BLUE");
   dma_display->fillScreenRGB888(0, 0, 255);
-  delay(1000);
+  delay(200);
 
-  Serial.println("Fill screen: Neutral White");
-  dma_display->fillScreenRGB888(64, 64, 64);
-  delay(1000);
-
-  Serial.println("Fill screen: black");
+  Serial.println("Fill screen: BLACK");
   dma_display->fillScreenRGB888(0, 0, 0);
-  delay(1000);
+  delay(200);
 
   Serial.println("Setup done...");
 }
 
 void onConnectionEstablished() {
-  client.subscribe(BASIC_TOPIC_SET "bri", [](const String & payload) {
+  client.subscribe(BASIC_TOPIC_SET "bri", [](const String &payload) {
     int value = strtol(payload.c_str(), 0, 10);
-    mqttBri = max(1, min(255, value));
-    dma_display->setBrightness8(mqttBri * on);
+    mqttBri = max(1, min(31, value));
+    dma_display->setBrightness8((mqttBri << 3) * on);
     client.publish(BASIC_TOPIC_STATUS "bri", String(mqttBri), MQTT_RETAINED);
   });
 
-  client.subscribe(BASIC_TOPIC_SET "on", [](const String & payload) {
+  client.subscribe(BASIC_TOPIC_SET "on", [](const String &payload) {
     boolean value = payload != "0";
     on = value;
-    dma_display->setBrightness8(mqttBri * on);
+    dma_display->setBrightness8((mqttBri << 3) * on);
     client.publish(BASIC_TOPIC_STATUS "on", String(on), MQTT_RETAINED);
   });
 
@@ -118,103 +115,67 @@ void onConnectionEstablished() {
 
   ArduinoOTA.begin();
 
-  pixelflutServer.begin();
-  pixelflutServer.setNoDelay(true);
+  server.begin();
+  server.setNoDelay(true);
+  Serial.print("Now listening to tcp://");
+  Serial.print(CLIENT_NAME);
+  Serial.print(":");
+  Serial.println(LISTEN_PORT);
 }
 
-void handlePixelflutInput(WiFiClient &client, String str) {
-  #ifdef PRINT_TO_SERIAL
-  Serial.print("Pixelflut Command: ");
-  Serial.println(str);
-  #endif
-
-  commands += 1;
-
-  str.toLowerCase();
-
-  if (str == "help") {
-    client.println("there is no help yet");
-  } else if (str == "size") {
-    char buffer[20];
-    if (snprintf(buffer, sizeof(buffer), "SIZE %d %d", PANE_WIDTH, PANE_HEIGHT) >= 0) {
-      client.println(buffer);
-    }
-  } else if (str.startsWith("px")) {
-    auto s = str.c_str();
-    s += 3;
-
-    char *ptr;
-
-    int16_t x = strtol(s, &ptr, 10);
-    int16_t y = strtol(ptr, &ptr, 10);
-
-    auto length = strnlen(ptr, 20);
-
-    if (length == 0) {
-      char buffer[20];
-      if (snprintf(buffer, sizeof(buffer), "PX %d %d unknown", x, y) >= 0) {
-        client.println(buffer);
-      }
-    } else if (length == 6 + 1) {
-      // normal hex color
-      char buffer[20];
-
-      strncpy(buffer, ptr + 1, 2);
-      auto red = strtoul(buffer, 0, 16);
-      strncpy(buffer, ptr + 3, 2);
-      auto green = strtoul(buffer, 0, 16);
-      strncpy(buffer, ptr + 5, 2);
-      auto blue = strtoul(buffer, 0, 16);
-
-      dma_display->drawPixelRGB888(x, y, red, green, blue);
-    } else {
-      client.println("colorcode not implemented. Use 6 digit hex color.");
-    }
-  } else {
-    client.println("unknown command. try help");
-  }
-}
-
-void pixelflutLoop() {
-  for (auto i = pixelflutClients.size(); i > 0; i--) {
-    auto client = pixelflutClients.get(i - 1);
+void pixelclientUpdateClients() {
+  for (auto i = clients.size(); i > 0; i--) {
+    auto client = clients.get(i - 1);
 
     if (!client.connected()) {
-      pixelflutClients.remove(i - 1);
+      clients.remove(i - 1);
 
-      #ifdef PRINT_TO_SERIAL
+#ifdef PRINT_TO_SERIAL
       Serial.print("Client left. Remaining: ");
-      Serial.println(pixelflutClients.size());
-      #endif
+      Serial.println(clients.size());
+#endif
     }
   }
 
-  while (pixelflutServer.hasClient()) {
-    auto client = pixelflutServer.available();
+  while (server.hasClient()) {
+    auto client = server.available();
     client.setNoDelay(true);
+    client.write(PANE_WIDTH);
+    client.write(PANE_HEIGHT);
     client.flush();
 
-    #ifdef PRINT_TO_SERIAL
+#ifdef PRINT_TO_SERIAL
     Serial.print("Client new: ");
     Serial.print(client.remoteIP().toString());
     Serial.print(":");
     Serial.println(client.remotePort());
-    #endif
+#endif
 
-    pixelflutClients.add(client);
+    clients.add(client);
   }
+}
 
-  for (auto i = 0; i < pixelflutClients.size(); i++) {
-    auto client = pixelflutClients.get(i);
-
-    String input;
-    while (client.available()) {
-      char c = client.read();
-      if (c == '\n') {
-        handlePixelflutInput(client, input);
-        input = "";
-      } else if (c >= 32) {
-        input += c;
+void pixelclientLoop() {
+  for (auto i = 0; i < clients.size(); i++) {
+    auto client = clients.get(i);
+    if (client.available()) {
+      uint8_t kind = client.read();
+      if (kind == 1) { // Fill
+        uint8_t red = client.read();
+        uint8_t green = client.read();
+        uint8_t blue = client.read();
+        commands += 1;
+        bytes += 4;
+        dma_display->fillScreenRGB888(red, green, blue);
+      } else if (kind == 2) { // Pixel
+        uint8_t x = client.read();
+        uint8_t y = client.read();
+        uint8_t red = client.read();
+        uint8_t green = client.read();
+        uint8_t blue = client.read();
+        commands += 1;
+        bytes += 6;
+        dma_display->drawPixelRGB888(x, y, red, green, blue);
       }
     }
   }
@@ -225,41 +186,57 @@ unsigned long nextMeasure = 0;
 
 void loop() {
   client.loop();
+  digitalWrite(LED_BUILTIN, client.isConnected() ? LOW : HIGH);
   ArduinoOTA.handle();
-  pixelflutLoop();
-  digitalWrite(LED_BUILTIN, client.isConnected() ? HIGH : LOW);
+  pixelclientUpdateClients();
 
   auto now = millis();
 
   if (client.isConnected()) {
-    if (lastPublishedClientAmount != pixelflutClients.size()) {
-      lastPublishedClientAmount = pixelflutClients.size();
+    if (lastPublishedClientAmount != clients.size()) {
+      lastPublishedClientAmount = clients.size();
       client.publish(BASIC_TOPIC_STATUS "clients", String(lastPublishedClientAmount), MQTT_RETAINED);
     }
 
     if (now > nextCommandsUpdate) {
       nextCommandsUpdate = now + 5000;
-      auto commands_per_second = commands / 5.0;
+      auto commands_per_second = commands / 5.0f;
       commands = 0;
       float avgCps = mkCommandsPerSecond.addMeasurement(commands_per_second);
-      #ifdef PRINT_TO_SERIAL
+#ifdef PRINT_TO_SERIAL
       Serial.print("Commands per Second:  ");
       Serial.print(String(commands_per_second).c_str());
       Serial.print("   Average: ");
       Serial.println(String(avgCps).c_str());
-      #endif
+#endif
+
+      auto kB_per_second = bytes / (1024.0f * 5.0f);
+      bytes = 0;
+      float avgKbps = mkKilobytesPerSecond.addMeasurement(kB_per_second);
+#ifdef PRINT_TO_SERIAL
+      Serial.print("Kilobytes per Second:  ");
+      Serial.print(String(kB_per_second).c_str());
+      Serial.print("   Average: ");
+      Serial.println(String(avgKbps).c_str());
+#endif
     }
 
     if (now > nextMeasure) {
       nextMeasure = now + 5000;
       long rssi = WiFi.RSSI();
       float avgRssi = mkRssi.addMeasurement(rssi);
-      #ifdef PRINT_TO_SERIAL
+#ifdef PRINT_TO_SERIAL
       Serial.print("RSSI        in dBm:     ");
       Serial.print(String(rssi).c_str());
       Serial.print("   Average: ");
       Serial.println(String(avgRssi).c_str());
-      #endif
+#endif
     }
+  }
+
+  auto nextMqtt = now + 450;
+  auto nextUpdate = min(min(nextMeasure, nextCommandsUpdate), nextMqtt);
+  while (millis() < nextUpdate) {
+    pixelclientLoop();
   }
 }
